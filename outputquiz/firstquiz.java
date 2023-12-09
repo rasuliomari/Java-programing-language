655e0a6ab72d23641606abd17015c370e9dcf282
rasuliomari <122861790+rasuliomari@users.noreply.github.com>
2023-12-09 23:11:19 +0000

    first quize that display hellow

---
+++ outputquiz/firstquiz.java
@@ -0,0 +1,8 @@
+public class firstquiz {
+    public static void anotherMethod(){
+        System.out.print("there");
+    }
+    public static void main(String[] args){
+        System.out.print("hellow");
+    }
+}
