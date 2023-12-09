203544637d6c5cd3fccb4d4d0208130f6d216c20
rasuliomari <122861790+rasuliomari@users.noreply.github.com>
2023-12-09 22:41:28 +0000

    first code of helloworld

---
+++ firstcode/helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+    static int year;
+    public static void main (String[] args){
+        year = 2023;
+        System.out.println(new String("hellow world") + year);
+    }
+}
