fa33514ca11ea21562b597d7cd6e68e96b32ca61
rasuliomari <122861790+rasuliomari@users.noreply.github.com>
2023-12-10 22:35:26 +0000

    updates

--- outputquiz/TestOperators.java
+++ outputquiz/TestOperators.java
@@ -2,7 +2,8 @@ public class TestOperators{
     public static void main(String[] args){
         int a=10; int b=20; int c=30; int d=40; int x=1;
         int e,f,g,h;
-        System.out.println("a="+a+ "b="+b+ "c="+c+ "d="+d);
+        System.out.println("a="+a+ "b="+b+ "c="+c+
+         "d="+d);
 
         if(a+d == b+c)
             System.out.println("a+d == b+c is true");
