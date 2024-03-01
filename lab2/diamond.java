194eb24ef834223a861de4dc3eaf965038035b65
rasuliomari <122861790+rasuliomari@users.noreply.github.com>
2024-03-01 05:18:10 +0000

    Asteric diamond like

---
+++ lab2/diamond.java
@@ -0,0 +1,35 @@
+public class diamond {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces before the first asterisk
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print asterisks
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces before the first asterisk
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print asterisks
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
